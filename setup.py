34d6755a72190d8e23cf1d919996e93048ba311c
omkar703 <omkarphadatareofficial30122004@gmail.com>
2025-08-27 20:40:43 +0530

    change dir

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'youtube',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'omkar',
+    author_email= 'omkarphadatare30122004@gmail@gmail.com',
     packages= find_packages(),
     install_requires = []
 
